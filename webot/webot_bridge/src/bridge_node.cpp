91b382031a1f0ae72f9500a69b6912acb2cb295d
happyrobotman <mail@e-dream-water.com>
2017-10-16 12:07:57 +0800

    Update bridge_node.cpp

--- webot/webot_bridge/src/bridge_node.cpp
+++ webot/webot_bridge/src/bridge_node.cpp
@@ -484,8 +484,8 @@ void callback(const geometry_msgs::Twist& cmd_vel)
     ROS_INFO("Linear Components:[%f,%f,%f]",cmd_vel.linear.x,cmd_vel.linear.y,cmd_vel.linear.z);
     ROS_INFO("Angular Components:[%f,%f,%f]",cmd_vel.angular.x,cmd_vel.angular.y,cmd_vel.angular.z);
 */
-    linear_x_speed = (int16_t)(680*cmd_vel.linear.x);
-    angular_speed = (int16_t)(680*cmd_vel.angular.z);
+    linear_x_speed = (int16_t)(700*cmd_vel.linear.x);
+    angular_speed = (int16_t)(700*cmd_vel.angular.z);
     payload_pointer = (uint16_t *)msg.payload64;
     *payload_pointer     = linear_x_speed ;
     *(payload_pointer+3) = angular_speed;
